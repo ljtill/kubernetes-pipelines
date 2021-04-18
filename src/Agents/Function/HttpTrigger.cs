59e1fa1cba35213542e63e88885335a0959958f0
Lyon Till <lyon.till@microsoft.com>
2021-04-18 01:42:28 +0100

    Update commit

---
+++ src/Agents/Function/HttpTrigger.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+using Internal.Utils;
+
+namespace Internal.Function
+{
+    public static class Agents
+    {
+        [FunctionName("agents")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            // Configuration
+            string resourceGroupName = SdkContext.RandomResourceName("rg-aci-", 6);
+
+            // Context
+            IAzure azure = Context.GetContext(log);
+
+            // Resource Groups
+            var resourceGroups = await Resources.GetResourceGroups(log, azure);
+
+            string responseMessage = "Sample response";
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
