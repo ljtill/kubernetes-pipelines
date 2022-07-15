d7509df80f691177961285efc6d620b7c7bb535e
Lyon Till <lyon.till@microsoft.com>
2022-07-15 13:31:30 +0100

    refactor: remove kubernetesclient abstraction

--- src/Pipelines.Runtime/Clients/Kubernetes/KubernetesClient.cs
+++
@@ -1,8 +0,0 @@
-namespace Pipelines.Runtime.Clients;
-
-public class KubernetesClient : k8s.Kubernetes
-{
-    public KubernetesClient(KubernetesClientConfiguration config, params DelegatingHandler[] handlers) : base(config, handlers)
-    {
-    }
-}
