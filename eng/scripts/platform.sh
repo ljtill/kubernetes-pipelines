06dc38194948e20abed35cd8b9be3c8eed6f5b7c
Lyon Till <lyon.till@microsoft.com>
2022-08-01 19:23:16 +0100

    refactor: develop end-to-end bootstrap process

--- eng/scripts/platform.sh
+++ eng/scripts/platform.sh
@@ -3,38 +3,19 @@
 set -e
 
 #
-# Variables
-#
-
-deployment_name="Microsoft.Bicep.Resources"
-location="Norway East"
-config_path="../configs/platform.local.json"
-
-#
 # Environment
 #
 
 environment()
 {
-    echo "=> Checking environment variables..."
+    echo "=> Loading runtime variables..."
 
-    if [[ -z "$TENANT_ID" ]]; then
-        echo "Missing required environment variable (TENANT_ID)"
-        exit 1
-    fi
-    echo "==> Reading variable - TENANT_ID :: $TENANT_ID"
-
-    if [[ -z "$RESOURCE_GROUP_NAME" ]]; then
-        echo "Missing required environment variable (RESOURCE_GROUP_NAME)"
-        exit 1
-    fi
-    echo "==> Reading variable - RESOURCE_GROUP_NAME :: $RESOURCE_GROUP_NAME"
-
-    if [[ -z "$CLUSTER_NAME" ]]; then
-        echo "Missing required environment variable (CLUSTER_NAME)"
-        exit 1
-    fi
-    echo "==> Reading variable - CLUSTER_NAME :: $CLUSTER_NAME"
+    tenant_id=$(az account show -o json | jq -r '.tenantId')
+    config_path="../configs/platform.local.json"
+    config_data=$(cat $config_path)
+    deployment_name="Microsoft.Bicep.Resources"
+    location="Norway East"
+    app_name="Pipelines"
 
     # TODO(ljtill): Check environment connection - kubectl & helm
 }
@@ -46,10 +27,24 @@ environment()
 deploy()
 {
     echo "=> Deploying platform..."
-    az deployment sub create \
-        --name "$deployment_name" \
-        --location "$location" \
-        --template-file "./region.bicep"
+
+    if [[ -z "$(az ad app list --display-name "$app_name" -o json | jq -r '.[]')" ]]; then
+        echo "==> Creating azure ad application..."
+        az ad app create --display-name "$app_name" -o none
+    else
+        echo "==> Skipping azure ad application creation..."
+    fi
+
+    if [[ -z "$(az ad sp list --display-name "$app_name" -o json | jq -r '.[]')" ]]; then
+        echo "==> Creating azure ad service principal..."
+        app_id=$(az ad app list --display-name "$app_name" -o json | jq -r '.[].appId')
+        az ad sp create --id "$app_id" -o none
+    else
+        echo "==> Skipping azure ad service principal creation..."
+    fi
+
+    echo "==> Deploying azure resources..."
+    az deployment sub create --name "$deployment_name" --location "$location" --template-file "./region.bicep"
 }
 
 #
@@ -60,56 +55,56 @@ delete()
 {
     echo "=> Deleting platform..."
 
-    echo "==> Removing endpoint resources..."
-    cat $config_path \
-        | jq -r ".endpoints.resourceGroup" \
-        | xargs -rtL1 az group delete --yes --name
-
     echo "==> Removing cluster resources..."
-    cat $config_path \
-        | jq -r ".clusters[].resourceGroup" \
-        | xargs -rtL1 az group delete --yes --name
+    echo "$config_data" | jq -r ".clusters[].resourceGroup" | xargs -rtL1 az group delete --yes --name
 
     echo "==> Removing service resources..."
-    cat $config_path \
-        | jq -r ".services.resourceGroup" \
-        | xargs -rtL1 az group delete --yes --name
+    echo "$config_data" | jq -r ".services.resourceGroup" | xargs -rtL1 az group delete --yes --name
 
-    echo "==> Removing zone resources..."
-    cat $config_path \
-        | jq -r ".zones.resourceGroup" \
-        | xargs -rtL1 az group delete --yes --name
+    if [[ -z "$(az ad app list --display-name "$app_name" -o json | jq -r '.[]')" ]]; then
+        echo "==> Deleting azure ad application..."
+        app_id=$(az ad app list --display-name "$app_name" -o json | jq -r '.[].appId')
+        az ad app delete --id "" -o none
+    else
+        echo "==> Skipping azure ad application deletion..."
+    fi
+
+    echo "=> Purging vaults..."
+    az keyvault list-deleted -o json | jq -r '.[].name' | xargs -rtL1 az keyvault purge --name
 }
 
-# TODO: Implement component removal
-# delete_autoscaler()
-# {
-#     echo "==> Uninstalling helm chart...."
-#     helm uninstall keda --namespace keda
-
-#     echo "==> Deleting namespace..."
-#     kubectl delete namespace keda
-# }
-
-# TODO: Implement component removal
-# delete_identity()
-# {
-#     echo "==> Uninstall helm chart..."
-#     helm uninstall workload-identity-webhook --namespace azure-workload-identity-system
-
-#     echo "==> Deleting service account..."
-#     kubectl delete serviceaccount workload-identity-sa -n functions
-# }
-
-#
-# Purge
-#
-
-purge()
+delete_autoscaler()
 {
-    echo "=> Purging vaults..."
-    az keyvault list-deleted --query "[].name" -o tsv \
-        | xargs -rtL1 az keyvault purge --name
+    if [[ -n "$(helm list --namespace keda-system -o json | jq -r '.[] | select(.name == "keda")')" ]]; then
+        echo "==> Uninstalling helm chart...."
+        helm uninstall keda --namespace keda-system
+    else
+        echo "==> Skipping helm chart installation..."
+    fi
+
+    if [[ -n "$(kubectl get namespace -o json | jq -r '.items[] | select(.metadata.name == "keda-system")')" ]]; then
+        echo "==> Deleting kubernetes namespace..."
+        kubectl delete namespace keda-system
+    else
+        echo "==> Skipping kubernetes namespace deletion..."
+    fi
+}
+
+delete_identity()
+{
+    if [[ -n "$(helm list --namespace workload-identity-system -o json | jq -r '.[] | select(.name == "workload-identity-webhook")')" ]]; then
+        echo "==> Uninstall helm chart..."
+        helm uninstall workload-identity-webhook --namespace azure-workload-identity-system
+    else
+        echo "==> Skipping helm chart installation..."
+    fi
+
+    if [[ -n "$(kubectl get namespace -o json | jq -r '.items[] | select(.metadata.name == "workload-identity-system")')" ]]; then
+        echo "==> Deleting kubernetes namespace..."
+        kubectl delete namespace workload-identity-system
+    else
+        echo "==> Skipping kubernetes namespace deletion..."
+    fi
 }
 
 #
@@ -119,10 +114,7 @@ purge()
 validate()
 {
     echo "=> Validating platform..."
-    az deployment sub what-if \
-        --name "$deployment_name" \
-        --location "$location" \
-        --template-file "./region.bicep"
+    az deployment sub what-if --name "$deployment_name" --location "$location" --template-file "./region.bicep"
 }
 
 #
@@ -132,6 +124,8 @@ validate()
 bootstrap_autoscaler()
 {
     echo -e "\n=> Bootstrapping kubernetes autoscaler..."
+
+    # Kubernetes
 
     if [[ -z "$(helm repo list -o json | jq -r '.[] | select(.name == "kedacore")')" ]]; then
         echo "==> Adding helm chart repo..."
@@ -162,6 +156,23 @@ bootstrap_identity()
 {
     echo -e "\n=> Bootstrapping azure workload identity..."
 
+    # Azure Active Directory
+
+    app_metadata=$(az ad app list --display-name "$app_name" -o json)
+    app_id=$(echo "$app_metadata" | jq -r ".[].appId")
+    object_id=$(echo "$app_metadata" | jq -r ".[].id")
+
+    if [[ -z "$(az rest --method GET --uri "https://graph.microsoft.com/beta/applications/$object_id/federatedIdentityCredentials" | jq -r '.value[]')" ]]; then
+        echo "==> Creating federated credentials..."
+        issuer_url=$(az aks show --name "$CLUSTER_NAME" --resource-group "$RESOURCE_GROUP_NAME" --query "oidcIssuerProfile.issuerUrl" -o tsv)
+        rest_body="{\"name\": \"kubernetes-federated-credential\", \"issuer\": \"$issuer_url\", \"subject\": \"system:serviceaccount:functions-system:functions\", \"description\": \"TBD\", \"audiences\": [\"api://AzureADTokenExchange\"]}"
+        az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$object_id/federatedIdentityCredentials" --body "$rest_body" -o none
+    else
+        echo "==> Skipping federated credentials creation..."
+    fi
+
+    # Kubernetes
+
     if [[ -z "$(helm repo list -o json | jq -r '.[] | select(.name == "azure-workload-identity")')" ]]; then
         echo "==> Adding helm chart repo..."
         helm repo add azure-workload-identity https://azure.github.io/azure-workload-identity/charts
@@ -181,57 +192,10 @@ bootstrap_identity()
 
     if [[ -z "$(helm list --namespace workload-identity-system -o json | jq -r '.[] | select(.name == "workload-identity-webhook")')" ]]; then
         echo "==> Installing helm chart..."
-        helm install workload-identity-webhook azure-workload-identity/workload-identity-webhook --namespace workload-identity-system --set azureTenantID="$TENANT_ID"
+        helm install workload-identity-webhook azure-workload-identity/workload-identity-webhook --namespace workload-identity-system --set azureTenantID="$tenant_id"
     else
         echo "==> Skipping helm chart installation..."
     fi
-
-    if [[ -z "$(kubectl get namespace -o json | jq -r '.items[] | select(.metadata.name == "functions-system")')" ]]; then
-        echo "==> Creating kubernetes namespace..."
-        kubectl create namespace functions-system
-    else
-        echo "==> Skipping kubernetes namespace creation..."
-    fi
-
-    if [[ -z "$(az ad app list --display-name "Pipelines" -o json | jq -r '.[]')" ]]; then
-        echo "==> Creating azure ad application..."
-        az ad app create --display-name "Pipelines" -o none
-    else
-        echo "==> Skipping azure ad application creation..."
-    fi
-
-    app_metadata=$(az ad app list --display-name "Pipelines" -o json)
-    app_id=$(echo "$app_metadata" | jq -r ".[].appId")
-    object_id=$(echo "$app_metadata" | jq -r ".[].id")
-
-    if [[ -z "$(az ad sp list --display-name "Pipelines" -o json | jq -r '.[]')" ]]; then
-        echo "==> Creating azure ad service principal..."
-        az ad sp create --id "$app_id" -o none
-    else
-        echo "==> Skipping azure ad service principal creation..."
-    fi
-
-    if [[ -z "$(kubectl get serviceaccount -n functions-system -o json | jq -r '.items[] | select(.metadata.name == "workload-identity-sa")')" ]]; then
-        echo "==> Creating kubernetes service account..."
-        kubectl create serviceaccount workload-identity-sa --namespace functions-system
-        kubectl annotate serviceaccount workload-identity-sa --namespace functions-system azure.workload.identity/client-id="$appId"
-        kubectl label serviceaccount workload-identity-sa --namespace functions-system azure.workload.identity/use=true
-    else
-        echo "==> Skipping kubernetes service account creation..."
-    fi
-
-    if [[ -z "$(az rest --method GET --uri "https://graph.microsoft.com/beta/applications/$object_id/federatedIdentityCredentials" | jq -r '.value[]')" ]]; then
-        echo "==> Creating federated credentials..."
-        issuer_url=$(az aks show --name "$CLUSTER_NAME" --resource-group "$RESOURCE_GROUP_NAME" --query "oidcIssuerProfile.issuerUrl" -o tsv)
-        rest_body="{\"name\": \"kubernetes-federated-credential\", \"issuer\": \"$issuer_url\", \"subject\": \"system:serviceaccount:functions-system:workload-identity-sa\", \"description\": \"TBD\", \"audiences\": [\"api://AzureADTokenExchange\"]}"
-        az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$object_id/federatedIdentityCredentials" --body "$rest_body" -o none
-    else
-        echo "==> Skipping federated credentials creation..."
-    fi
-
-    # TODO(ljtill): Assign Service Principal to Service Bus Queues -- Receiver / Sender
-    # TODO(ljtill): Assign Service Principal to Key Vault
-    # TODO(ljtill): Assign Service Princiapl to Storage Account
 }
 
 #
@@ -245,9 +209,11 @@ case $command in
         environment
         ;;
     "deploy")
+        environment
         deploy
         ;;
     "validate")
+        environment
         validate
         ;;
     "bootstrap")
@@ -256,6 +222,7 @@ case $command in
         bootstrap_identity
         ;;
     "delete")
+        environment
         delete
         ;;
     "purge")
