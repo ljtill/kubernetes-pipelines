7589bde618658b49d199d30334b3f7dd2822206e
Lyon Till <lyon.till@microsoft.com>
2022-08-15 10:34:51 +0100

    chore: powershell linter

--- eng/scripts/runtime.ps1
+++ eng/scripts/runtime.ps1
@@ -1,50 +1,43 @@
-## ------------------------------------------------------------------------
-## Input Parameters
-## 
-## ------------------------------------------------------------------------
-
 param(
-  [Parameter()]
-  [String]$Action
-) 
+    [Parameter()]
+    [String]$Action
+)
 
 #
 # Environment
 #
 
-Function Environment
-{
+function Environment {
     Write-Output "=> Loading configuration variables..."
 
     # TODO: Handle multiple cluster configurations
     # TODO: Handle multiple applications
 
-    $configData=$(Get-Content ../../eng/configs/platform.local.json) | ConvertFrom-Json
-    $serviceName=$configData.services.name
-    $clusterName=$configData.clusters[0].name
-    $appName="Pipelines2"
-    $appId=$(az ad app list --display-name $appName -o json | ConvertFrom-Json)[0].appId
+    $configData = $(Get-Content ../../eng/configs/platform.local.json) | ConvertFrom-Json
+    $serviceName = $configData.services.name
+    $clusterName = $configData.clusters[0].name
+    $appName = "Pipelines2"
+    $appId = $(az ad app list --display-name $appName -o json | ConvertFrom-Json)[0].appId
 
     $props = @{
         ServiceName = $serviceName
-        ConfigPath = $configPath
-        ConfigData = $configData
+        ConfigPath  = $configPath
+        ConfigData  = $configData
         ClusterName = $clusterName
-        AppId = $appId
-        AppName = $appName
-        }
+        AppId       = $appId
+        AppName     = $appName
+    }
 
-        $envVariables = new-object psobject -Property $props
+    $envVariables = new-object psobject -Property $props
 
-        return $envVariables
+    return $envVariables
 }
 
 #
 # Login
 #
 
-Function Login($environmentVariables)
-{
+function Login($environmentVariables) {
     Write-Output "=> Authenticating session..."
     az acr login --name $environmentVariables.ServiceName
 }
@@ -53,8 +46,7 @@ Function Login($environmentVariables)
 # Build
 #
 
-Function Build($environmentVariables)
-{
+function Build($environmentVariables) {
     Set-Location ../../src/Pipelines.Runtime
 
     Write-Output "\n=> Building runtime..."
@@ -68,8 +60,7 @@ Function Build($environmentVariables)
 # Generate
 #
 
-Function Generate($environmentVariables)
-{
+function Generate($environmentVariables) {
     Set-Location ../../src/Pipelines.Runtime
     Write-Output "=> Generating deployment files..."
 
@@ -81,18 +72,17 @@ Function Generate($environmentVariables)
 
     Write-Output "==> Replacing manifest value..."
 
-    (Get-Content functions.local.yaml) -replace "<StorageAccountName>","$($environmentVariables.ClusterName)" | Set-Content functions.local.yaml
-    (Get-Content functions.local.yaml) -replace "<ServiceBusName>","$($environmentVariables.ClusterName)" | Set-Content functions.local.yaml
-    (Get-Content functions.local.yaml) -replace "<RegistryName>","$($environmentVariables.ServiceName)" | Set-Content functions.local.yaml
-    (Get-Content functions.local.yaml) -replace "<ClientId>","$($environmentVariables.AppId)" | Set-Content functions.local.yaml
+    (Get-Content functions.local.yaml) -replace "<StorageAccountName>", "$($environmentVariables.ClusterName)" | Set-Content functions.local.yaml
+    (Get-Content functions.local.yaml) -replace "<ServiceBusName>", "$($environmentVariables.ClusterName)" | Set-Content functions.local.yaml
+    (Get-Content functions.local.yaml) -replace "<RegistryName>", "$($environmentVariables.ServiceName)" | Set-Content functions.local.yaml
+    (Get-Content functions.local.yaml) -replace "<ClientId>", "$($environmentVariables.AppId)" | Set-Content functions.local.yaml
 }
 
 #
 # Push
 #
 
-Function Push($environmentVariables)
-{
+function Push($environmentVariables) {
     Set-Location ../../src/Pipelines.Runtime
 
     Write-Output "=> Pushing image..."
@@ -103,8 +93,7 @@ Function Push($environmentVariables)
 # Clean
 #
 
-Function Clean
-{
+Function Clean {
     Set-Location ../../src/Pipelines.Runtime
 
     Write-Output "=> Cleaning..."
@@ -115,8 +104,7 @@ Function Clean
 # Run
 #
 
-Function Run
-{
+function Run {
     Set-Location ../../src/Pipelines.Runtime
 
     Write-Output "=> Starting functions host..."
@@ -127,35 +115,30 @@ Function Run
 # Deploy
 #
 
-Function Deploy
-{
+function Deploy {
     Set-Location ../../src/Pipelines.Runtime
 
     Write-Output "=> Deploying runtime..."
 
-    $checkKubeNS=$(kubectl get namespace -o json | ConvertFrom-Json).items | Where-Object {$_.metadata.name -eq "functions-system"}
+    $checkKubeNS = $(kubectl get namespace -o json | ConvertFrom-Json).items | Where-Object { $_.metadata.name -eq "functions-system" }
 
-    if(!$checkKubeNS)
-    {
+    if (!$checkKubeNS) {
         Write-Output "==> Creating kubernetes namespace..."
         kubectl create namespace functions-system
-    }   
-    else
-    {
+    }
+    else {
         Write-Output "==> Skipping kubernetes namespace creation..."
     }
 
-    $checkKubeDeployment=$(kubectl get deployment -n functions-system -o json | ConvertFrom-Json).items | Where-Object {$_.metadata.name -eq "functions"}
+    $checkKubeDeployment = $(kubectl get deployment -n functions-system -o json | ConvertFrom-Json).items | Where-Object { $_.metadata.name -eq "functions" }
 
-    if(!$checkKubeDeployment)
-    {
+    if (!$checkKubeDeployment) {
         Write-Output "==> Creating kubernetes deployment..."
         kubectl apply -f ./functions.yaml
         # NOTE: Following command is included as an archive for future reference
         #func kubernetes deploy --name "$runtime" --image-name "runtime/$runtime" --registry "$REGISTRY_NAME.azurecr.io/runtime" --min-replicas 1 --namespace "$namespace"
-    } 
-    else
-    {
+    }
+    else {
         Write-Output "==> Skipping kubernetes deployment creation..."
     }
 }
@@ -164,32 +147,27 @@ Function Deploy
 # Delete
 #
 
-Function Delete
-{
+function Delete {
     Write-Output "=> Destroying runtime..."
 
-    $checkKubeDeployment=$(kubectl get deployment -n functions-system -o json  | ConvertFrom-Json).items | Where-Object {$_.metadata.name -eq "functions"}
+    $checkKubeDeployment = $(kubectl get deployment -n functions-system -o json  | ConvertFrom-Json).items | Where-Object { $_.metadata.name -eq "functions" }
 
-    if($checkKubeDeployment)
-    {
+    if ($checkKubeDeployment) {
         Write-Output "==> Deleting kubernetes deployment..."
         kubectl delete -f ./functions.yaml
         # NOTE: Following command is included as an archive for future reference
         #func kubernetes delete --name "$runtime" --image-name "runtime/$runtime" --registry "$REGISTRY_NAME.azurecr.io/runtime" --namespace "$namespace"
-    } 
-    else
-    {
+    }
+    else {
         Write-Output "==> Skipping kubernetes deployment deletion..."
     }
-    
-    $checkKubeNS=$(kubectl get namespace -o json  | ConvertFrom-Json).items | Where-Object {$_.metadata.name -eq "functions-system"}
-    if($checkKubeNS)
-    {
+
+    $checkKubeNS = $(kubectl get namespace -o json  | ConvertFrom-Json).items | Where-Object { $_.metadata.name -eq "functions-system" }
+    if ($checkKubeNS) {
         Write-Output "==> Deleting kubernetes namespace..."
         kubectl delete namespace functions-system
     }
-    else
-    {
+    else {
         Write-Output "==> Skipping kubernetes namespace deletion..."
     }
 }
@@ -198,15 +176,14 @@ Function Delete
 # Invocation
 #
 
-# TODO: Need to find a way to call it like "make deploy"
 switch ($Action) {
-    "Login" { Write-Output "Environment Variables Set"; $envVariables=Environment; Login($envVariables) }
-    "Build" { Write-Output "Environment Variables Set"; $envVariables=Environment; Build($envVariables) }
-    "Generate" { Write-Output "Environment Variables Set"; $envVariables=Environment; Generate($envVariables)}
-    "Push" { Write-Output "Environment Variables Set"; $envVariables=Environment; Push($envVariables)}
-    "Clean" { Write-Output "Environment Variables Set"; $envVariables=Environment; Clean}
-    "Run" { Write-Output "Environment Variables Set"; $envVariables=Environment; Run}
-    "Deploy" { Write-Output "Environment Variables Set"; $envVariables=Environment; Deploy }
-    "Delete" { Write-Output "Environment Variables Set"; $envVariables=Environment; Delete }
-     Default { Write-Output "Missing argument"}
+    "Login" { Write-Output "Environment Variables Set"; $envVariables = Environment; Login($envVariables) }
+    "Build" { Write-Output "Environment Variables Set"; $envVariables = Environment; Build($envVariables) }
+    "Generate" { Write-Output "Environment Variables Set"; $envVariables = Environment; Generate($envVariables) }
+    "Push" { Write-Output "Environment Variables Set"; $envVariables = Environment; Push($envVariables) }
+    "Clean" { Write-Output "Environment Variables Set"; $envVariables = Environment; Clean }
+    "Run" { Write-Output "Environment Variables Set"; $envVariables = Environment; Run }
+    "Deploy" { Write-Output "Environment Variables Set"; $envVariables = Environment; Deploy }
+    "Delete" { Write-Output "Environment Variables Set"; $envVariables = Environment; Delete }
+    Default { Write-Output "Missing argument" }
 }
